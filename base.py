58428a51cc443247c37e081ce45d90210313fd8a
Lucas Schwaemmle Ruaro <47819585+LucasRuaro@users.noreply.github.com>
2019-06-07 09:59:57 -0300

    Add files via upload
    aula

--- base.py
+++ base.py
@@ -1,67 +1,48 @@
 import peewee, os
+from peewee import *
 
-db = peewee.MySQLDatabase('sys', user='root', password='root')
+db = MySQLDatabase('teste', user='root', password='root')
 
-class Cliente(peewee.Model):
-    nome_cliente = peewee.CharField()
-    cpf = peewee.CharField()
-
-    class Meta:
-        database = db
-
-    def __str__(self):
-        return self.nome_cliente + ", cpf:" + self.cpf
-
-class Animal(peewee.Model):
-    nome_pet = peewee.CharField()
-    tipo_animal = peewee.CharField()
-    raca = peewee.CharField()
-    cliente = peewee.ForeignKeyField(Cliente)
+class BaseModel(Model):
 
     class Meta:
         database = db
 
-    def __str__(self):
-        return self.nome_pet + ", " + self.tipo_animal + ", " + self.raca + " de " +str(self.cliente)
+class Produto(BaseModel):
+    nome = CharField()
+    #Preço unitário ou por quilo
+    preco = FloatField()
 
-class Consulta(peewee.Model):
-    data = peewee.CharField()
-    servico = peewee.CharField()
-    horario = peewee.CharField()
-    animal = peewee.ForeignKeyField(Animal)
-    confirma = peewee.CharField()
-    myID = peewee.CharField()
+class Carrinho(BaseModel):
+    tempo = DateTimeField()
 
-    class Meta:
-        database = db
-
-    def __str__(self):
-        return self.servico+" em "+self.data+":"+self.horario+", confirmado: "+\
-        self.confirma+", ID da consulta: "+self.myID+" | animal: "+str(self.animal)
+class ItemCarrinho(BaseModel):
+    #qtd pode ser em quilo ou unitário
+    qtd_item = CharField()
+    produto = ForeignKeyField(Produto)
+    carrinho = ForeignKeyField(Carrinho)
 
 if __name__ == '__main__':
     try:
         db.connect()
-        db.create_tables([Cliente,Animal,Consulta], safe=True)
-        q = Consulta.delete()
+        db.create_tables([Produto, Carrinho, ItemCarrinho], safe=True)
+        q = ItemCarrinho.delete()
         q.execute()
-        q = Animal.delete()
+        q = Carrinho.delete()
         q.execute()
-        q = Cliente.delete()
+        q = Produto.delete()
         q.execute()
-    except peewee.OperationalError as e:
+    except OperationalError as e:
         print("erro ao criar tabelas: "+str(e))
 
     """print("TESTE DO ANIMAL")
     cli1 = Cliente(nome_cliente="José", cpf="15486354287")
     ani1 = Animal(nome_pet="Zed", tipo_animal="Dog", raca="Chiuaua", cliente=cli1)
     print(ani1)
-
     print("TESTE DA CONSULTA")
     con1 = Consulta(data="19/09/2018", servico="Consulta de rotina", 
     horario="14:00", animal=ani1, confirma="N", myID="c9d8f7gu4h3hnwsik3e")
     print(con1)
-
     print("TESTE DA PERSISTÊNCIA")
     cli1.save()
     ani1.save()
